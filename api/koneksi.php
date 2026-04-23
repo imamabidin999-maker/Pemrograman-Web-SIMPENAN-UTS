bfd61c4659339057cee80d4f6a49958d946d91fb
imamabidin999-maker <imamabidin999@gmail.com>
2026-04-23 16:30:39 +0700

    :wq

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,4 +1,26 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "p1-pemweb");
-if (!$koneksi) die("Koneksi Gagal: " . mysqli_connect_error());
+$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
+$port = 4000;
+$user = '2Vzj4pSZjsdQK7C.root';
+$pass = 'QGg7Imvucz85WjP0';
+$db   = 'pemweb-SIMPENAN';
+
+$koneksi = mysqli_init();
+
+mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
+
+$real_connect = mysqli_real_connect(
+    $koneksi, 
+    $host, 
+    $user, 
+    $pass, 
+    $db, 
+    $port, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+);
+
+if (!$real_connect) {
+    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
+}
 ?>
