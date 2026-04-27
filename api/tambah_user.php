29518211d458cc9ceff1f489b19ea0c62ba79c12
imamabidin999-maker <imamabidin999@gmail.com>
2026-04-28 04:11:00 +0700

    bismillah 11

--- api/tambah_user.php
+++ api/tambah_user.php
@@ -8,11 +8,11 @@ if(!isset($_COOKIE['role']) || $_COOKIE['role'] !== 'admin') {
 
 if(isset($_POST['username']) && isset($_POST['password'])) { 
     $username = mysqli_real_escape_string($koneksi, $_POST['username']);
-    
+    $email    = mysqli_real_escape_string($koneksi, $_POST['email'] ?? '');
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-    $role = 'user';
+    $role     = 'user';
 
-    $query = "INSERT INTO users (username, password, role) VALUES ('$username', '$password', '$role')";
+    $query = "INSERT INTO users (username, email, password, role) VALUES ('$username', '$email', '$password', '$role')";
     
     if(mysqli_query($koneksi, $query)) {
         header("Location: /admin/kelola_user.php");
